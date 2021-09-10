e456a45a0fd20fd146bc6a406d3a6cc51e456692
Malik Ata <Malikmajedata@gmail.com>
2021-09-10 11:11:38 -0700

    Setting up HTTP request

---
+++ src/main/java/io/javabrains/coronovirustrakcer/CoronovirusTrakcerApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.coronovirustrakcer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronovirusTrakcerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronovirusTrakcerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/javabrains/coronovirustrakcer/CoronovirusTrakcerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.coronovirustrakcer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronovirusTrakcerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
